89362c3bb2c0c5783b1d315d399e976ae161bb74
Josh Romero <joshr@nvidia.com>
2026-04-29 11:31:48 -0700

    Fix cudecompInit return in Fortran. Add some missing error checks during autotuning.
    Signed-off-by: Josh Romero <joshr@nvidia.com>

--- src/cudecomp.cc
+++ src/cudecomp.cc
@@ -590,15 +590,8 @@ cudecompResult_t cudecompFinalize(cudecompHandle_t handle) {
 }
 
 cudecompResult_t cudecompInit_F(cudecompHandle_t* handle_in, MPI_Fint mpi_comm_f) {
-  using namespace cudecomp;
-  try {
-    MPI_Comm mpi_comm = MPI_Comm_f2c(mpi_comm_f);
-    cudecompInit(handle_in, mpi_comm);
-  } catch (const cudecomp::BaseException& e) {
-    std::cerr << e.what();
-    return e.getResult();
-  }
-  return CUDECOMP_RESULT_SUCCESS;
+  MPI_Comm mpi_comm = MPI_Comm_f2c(mpi_comm_f);
+  return cudecompInit(handle_in, mpi_comm);
 }
 
 cudecompResult_t cudecompGridDescCreate(cudecompHandle_t handle, cudecompGridDesc_t* grid_desc_in,
